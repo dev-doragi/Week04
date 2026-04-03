48b3cde474cb7716ee017ed303759f62e5bcc12b
DDATMOUSE00 <ddatmouse00@gmail.com>
2026-04-03 14:01:39 +0900

    save-2
    save-2

---
+++ Assets/01.Scripts/Managers/TitleUIManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleUIManager : MonoBehaviour
+{
+    [Header("Scene Settings")]
+    [SerializeField] private string gameSceneName = "02.TestMainScene";
+
+    // 게임 시작
+    public void OnClickStart()
+    {
+        SceneManager.LoadScene(gameSceneName);
+    }
+
+    // 게임 종료
+    public void OnClickQuit()
+    {
+        Application.Quit();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
