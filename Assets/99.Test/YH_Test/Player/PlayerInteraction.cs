167055fb8da4e2a41a8549a3fc519eae02a2c4d1
Denise C <86213644+YeHyunCho@users.noreply.github.com>
2026-04-04 17:20:25 +0900

    플레이어 아이템 기본 픽드랍 추가

--- Assets/99.Test/YH_Test/Player/PlayerInteraction.cs
+++ Assets/99.Test/YH_Test/Player/PlayerInteraction.cs
@@ -3,11 +3,34 @@ using DG.Tweening;
 
 public class PlayerInteraction : MonoBehaviour
 {
+    [Header("좌클릭(도끼)")]
     public float swingAngle = 80f;
     public float swingDuration = 0.2f;
     public float returnDuration = 0.15f;
 
     private bool _isSwinging = false;
+
+    [Header("F키_아이템 줍기")]
+    public GameObject axeOverlay;
+    public GameObject currentItemOverlay;
+    public float pickupRange = 2f;
+    public LayerMask itemLayer;
+
+    private GameObject _heldItem;
+    private Camera _mainCamera;
+
+    private int _overlayLayer;
+    private int _defaultLayer;
+
+    private void Start()
+    {
+        _mainCamera = Camera.main;
+        _overlayLayer = LayerMask.NameToLayer("ViewModel");
+        _defaultLayer = LayerMask.NameToLayer("Interact");
+
+        currentItemOverlay = axeOverlay;
+    }
+
 
     public void BoatBreaker(Transform axe)
     {
@@ -34,5 +57,58 @@ public class PlayerInteraction : MonoBehaviour
             .SetEase(Ease.InOutQuad));
 
         sequence.OnComplete(() => _isSwinging = false);
+    }
+
+    public void Interact()
+    {
+        // if (아이템 근처면 줍기) else (불에 넣기) else (젖은 나무 말리기)
+        TryPickUp();
+    }
+
+    public void TryPickUp()
+    {
+        Ray ray = _mainCamera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
+
+        Debug.DrawRay(ray.origin, ray.direction * pickupRange, Color.red, 1f);
+
+        if (Physics.Raycast(ray, out RaycastHit hit, pickupRange, itemLayer))
+        {
+            Debug.DrawRay(ray.origin, ray.direction * hit.distance, Color.green, 1f);
+            PickUpItem(hit.collider.gameObject);
+        }
+    }
+
+    private void PickUpItem(GameObject item)
+    {
+        axeOverlay.gameObject.SetActive(false);
+
+        item.layer = _overlayLayer;
+        item.transform.position = new Vector3(-0.831f, 1.04f, -2.665f);
+        item.GetComponent<Rigidbody>().isKinematic = true;
+        item.GetComponent<Rigidbody>().useGravity = false;
+        item.GetComponent<BoxCollider>().isTrigger = true;
+
+        currentItemOverlay = item;
+        //currentItemOverlay.SetActive(true);
+
+        _heldItem = item;
+    }
+
+    public void DropItem()
+    {
+        if (_heldItem == null) return;
+
+        _heldItem.layer = _defaultLayer;
+        _heldItem.GetComponent<Rigidbody>().isKinematic = false;
+        _heldItem.GetComponent<Rigidbody>().useGravity = true;
+        _heldItem.GetComponent<BoxCollider>().isTrigger = false;
+
+        _heldItem.transform.position = transform.position + transform.forward * 1f;
+        _heldItem.GetComponent<Rigidbody>().AddForce(transform.forward * 2f + Vector3.up * 1f, ForceMode.Impulse);
+
+        currentItemOverlay = axeOverlay;
+        _heldItem = null;
+
+        axeOverlay.SetActive(true);
     }
 }
