ff18b5fe5f0e760dcbfea5b7d15601b9b71c1426
thlee99813@gmail.com <thlee99813@gmail.com>
2026-04-05 14:58:32 +0900

    [이태환] 디버거, 암초 개선

--- Assets/01.Scripts/Frame/Debugger.cs
+++ Assets/01.Scripts/Frame/Debugger.cs
@@ -1,11 +1,17 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using Unity.Cinemachine;
-public class DebugCamera : MonoBehaviour
+public class Debugger : MonoBehaviour
 {
     public CinemachineCamera[] cams;
     public int activePriority = 20;
     public int inactivePriority = 0;
+
+    public bool IsSteering = false;
+
+    [Header("Debug Target")]
+    public GameObject Boat;
+    public GameObject Player;
 
     public int CurrentCameraIndex { get; private set; }
 
@@ -13,45 +19,42 @@ public class DebugCamera : MonoBehaviour
     private void Update()
     {
 
-        if (Keyboard.current.digit0Key.wasPressedThisFrame)
+        if (Keyboard.current.yKey.wasPressedThisFrame) 
         {
-            ActivateCamera(0);
-
+            Boat.GetComponent<Boat>().GameStart = true;
         }
-        else if (Keyboard.current.digit1Key.wasPressedThisFrame)
+        else if (Keyboard.current.hKey.wasPressedThisFrame)
         {
-            ActivateCamera(1);
-
+            if(IsSteering == false)
+            {
+                IsControllSteerTrigger(true);
+            }
+            else
+            {
+                IsControllSteerTrigger(false);
+            }
         }
-        else if (Keyboard.current.digit2Key.wasPressedThisFrame)
-        {
-            ActivateCamera(2);
-
-        }
-        else if (Keyboard.current.digit3Key.wasPressedThisFrame)
-        {
-            ActivateCamera(3);
-
-        }
-        else if (Keyboard.current.digit4Key.wasPressedThisFrame)
-        {
-            ActivateCamera(4);
-
-        }
-   
-        else if (Keyboard.current.digit5Key.wasPressedThisFrame)
-        {
-            ActivateCamera(5);
-        }
+        else if (Keyboard.current.digit1Key.wasPressedThisFrame) ActivateCamera(0);
+        else if (Keyboard.current.digit2Key.wasPressedThisFrame) ActivateCamera(1);
+        else if (Keyboard.current.digit3Key.wasPressedThisFrame) ActivateCamera(2);
+        else if (Keyboard.current.digit4Key.wasPressedThisFrame) ActivateCamera(3);
+        else if (Keyboard.current.digit5Key.wasPressedThisFrame) ActivateCamera(4);
     }
     public void ActivateCamera(int index)
     {
+        if(index >= cams.Length) return;
         CurrentCameraIndex = index;
 
         for (int i = 0; i < cams.Length; i++)
         {
             cams[i].Priority = (i == index) ? activePriority : inactivePriority;
         }
+    }
+    public void IsControllSteerTrigger(bool iscontrol)
+    {
+        Boat.GetComponent<BoatSteeringController>().ControllSteer = iscontrol;
+        Player.GetComponent<PlayerEntity>().InputLock = iscontrol;
+        IsSteering = iscontrol;
     }
 
 
